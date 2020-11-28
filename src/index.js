4daaa341f0f3b8ca2e7473fee46d27c3cf3bdcc2
Harshith Venkatesh <44545218+Harshi7016@users.noreply.github.com>
2020-11-28 12:22:24 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -5,27 +5,33 @@ function generateQuote() {
     try {
       // fetch data from a url endpoint
       const response = await fetch("https://type.fit/api/quotes");
-      const image = await fetch("https://source.unsplash.com/1600x900/?nature");
+
+      //To fetch image from the url endpoint
+      //const image = await fetch("https://source.unsplash.com/1600x900/?nature");
       const data = await response.json();
-      //return data;
-      return { data, image };
+
+      //return quote data
+      return data;
+
+      //return quote,image
+      //return { data, image };
     } catch (error) {
       console.error(error); // catches both errors
     }
   }
 
   asyncFunc().then((result) => {
-    // let randomQuoteIndex = Math.floor(Math.random() * result.length);
-    // document.querySelector("#quote").innerHTML = `
-    // <p>${result[randomQuoteIndex].text}</p>
-    // `;
-
-    const { data, image } = result;
-    const randomQuoteIndex = Math.floor(Math.random() * data.length);
-    document.body.style.backgroundImage = `url("${image.url}")`;
+    let randomQuoteIndex = Math.floor(Math.random() * result.length);
     document.querySelector("#quote").innerHTML = `
-    <p>${result?.data[randomQuoteIndex].text}</p>
+    <p>${result[randomQuoteIndex].text}</p>
     `;
+
+    // const { data, image } = result;
+    // const randomQuoteIndex = Math.floor(Math.random() * data.length);
+    // document.body.style.backgroundImage = `url("${image.url}")`;
+    // document.querySelector("#quote").innerHTML = `
+    // <p>${result?.data[randomQuoteIndex].text}</p>
+    // `;
   });
 }
 generateQuote();
